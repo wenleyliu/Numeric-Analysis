c7cd70ff83544ccd83087fe78d5297d1c59ef25e
Wenley Liu <wenleyliu@gmail.com>
2020-10-30 14:47:35 +0800

    Update and rename bezier to bezierSpline.m

--- bezierSpline.m
+++ bezierSpline.m
@@ -1,16 +1,16 @@
-function [x,y] = bezier(X,Y,t)
-%�˵�
+function [x,y] = bezierSpline(X,Y,t)
+%the endpoint
 x1 = X(1)
 x2 = X(2)
 y1 = Y(1)
 y2 = Y(2)
-%���Ƶ�
+%the control point
 x3 = X(3)
 x4 = X(4)
 y3 = Y(3)
 y4 = Y(4)
 
-% ��������������
+% bezier spline function
 bx = 3*(x2-x1);
 by = 3*(y2-y1);
 cx = 3*(x3-x2)-bx;
